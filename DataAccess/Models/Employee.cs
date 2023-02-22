c55522fc9ae612f537dde6bfb650073383eb3e8e
Meysam <devops.meysam@gmail.com>
2023-02-22 13:49:41 -0800

    Implements Models and OOP

---
+++ DataAccess/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Employee : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ulong PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public Department Department { get; set; }
+        public decimal BaseSalary { get; set; }
+        public string GetBasicInfo()
+        {
+            string finalStr = FirstName + " " + LastName +
+                "\nCellphone: " + PhoneNumber +
+                "\nAddress: " + Address +
+                "\nDepartment: " + Department +
+                "\nBase Salary: " + BaseSalary;
+
+            return finalStr;
+        }
+    }
+    public enum Department
+    {
+        Production,
+        Sales,
+        Advertisement,
+        Management
+    }
+}
---
+++ DataAccess/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Product : IProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Producer { get; set; }
+        public decimal Price { get; set; }
+        public int AvailableCount { get; set; }
+        public string GetBasicInfo()
+        {
+            string finalStr = Name + 
+                "\nProducer: " + Producer + 
+                "\nPrice: $" + Price + 
+                "\nAvailable in stock: " + AvailableCount;
+            
+            return finalStr;
+        }
+    }
+}
