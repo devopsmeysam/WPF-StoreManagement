f9a3104adf4a44447a6ae8e1ebd78d5dce9b7d26
Meysam <devops.meysam@gmail.com>
2023-02-23 11:07:00 -0800

    Implements Local Temporary Data Access

---
+++ DataAccess/EmployeeDataAccess.cs
@@ -0,0 +1,70 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class EmployeeDataAccess
+    {
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+
+        public EmployeeDataAccess()
+        {
+            ReadEmployees();
+        }
+
+        private void ReadEmployees()
+        {
+            Employee emp1 = new Employee()
+            {
+                Id = 1,
+                FirstName = "Meysam",
+                LastName = "Mahdavikhansari",
+                PhoneNumber = 6476748899,
+                Address = "ABC Bayview Ave, Toronto, A0A 0A0",
+                Department = Department.Management,
+                BaseSalary = 75000
+            };
+            Employee emp2 = new Employee()
+            {
+                Id = 2,
+                FirstName = "Zara",
+                LastName = "NSRBZGRD",
+                PhoneNumber = 6476471100,
+                Address = "XYZ Yonge St, Toronto, Z9Z 9Z9",
+                Department = Department.Production,
+                BaseSalary = 85000
+            };
+            Employees.Add(emp1);
+            Employees.Add(emp2);
+        }
+
+        public void CreateEmployee(Employee emp)
+        {
+            Employees.Add(emp);
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            Employee employee = Employees.First(x => x.Id == id);
+            Employees.Remove(employee);
+        }
+
+        public void UpdateEmployee(Employee emp)
+        {
+            Employee employee = Employees.First(x => x.Id == emp.Id);
+            int index = Employees.IndexOf(employee);
+            Employees[index] = emp;
+        }
+
+        public int GetNextId()
+        {
+            int index = Employees.Any() ? Employees.Max(x => x.Id) + 1 : 1;
+
+            return index;
+        }
+    }
+}
---
+++ DataAccess/ProductDataAccess.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ProductDataAccess
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public ProductDataAccess()
+        {
+            ReadProducts();
+        }
+
+        public void CreateProduct(Product product)
+        {
+            Products.Add(product);
+        }
+
+        private void ReadProducts()
+        {
+            Product product01 = new Product()
+            {
+                Id = 1,
+                Name = "Interuniversalism",
+                Producer = "Mohammad Ali Taheri",
+                Price = 18,
+                AvailableCount = 22
+            };
+            Product product02 = new Product()
+            {
+                Id= 2,
+                Name = "It Snows In This House",
+                Producer = "Hamed Esmaeilion",
+                Price = 20,
+                AvailableCount = 28
+            };
+            Products.Add(product01);
+            Products.Add(product02);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            Product temp = Products.First(x =>  x.Id == product.Id);
+            int index = Products.IndexOf(temp);
+            Products[index] = product;
+        }
+
+        public void DeleteProduct(int id)
+        {
+            Product temp = Products.First(x => x.Id == id);
+            Products.Remove(temp);
+        }
+
+        public int GetNextId()
+        {
+            int index = Products.Any() ? Products.Max(x => x.Id) + 1 : 1;
+            
+            return index;
+        }
+    }
+}
